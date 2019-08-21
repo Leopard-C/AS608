446cf04d92a4389450b15674bcca65ff9c8a41e6
Leopard-C <leopard.c@outlook.com>
2019-08-21 14:14:34 +0800

    LICENSE

--- example/utils.c
+++ example/utils.c
@@ -1,5 +1,5 @@
 /*
-  Copyright (c) 2019  Leopad-C
+  Copyright (c) 2019  Leopard-C
 
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files (the "Software"), to deal
